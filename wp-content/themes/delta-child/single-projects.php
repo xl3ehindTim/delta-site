0dd704844b9320422d07d18965ab9b749ce054a4
Tim <timbrouwers1@gmail.com>
2024-05-08 15:53:21 +0200

    Added carousel

--- wp-content/themes/delta-child/single-projects.php
+++ wp-content/themes/delta-child/single-projects.php
@@ -8,71 +8,136 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 get_header(); ?>
 
 <?php
-    $project_id = get_the_ID();
-    $title = get_field('title');
-    $subtitle = get_field('subtitle');
-    $heroImage = get_field('hero_image');
-    $description = get_field('description');
-    $website = get_field('website');
-    $logo = get_field('logo');
-    $students = get_field('students', $project_id);
+$project_id = get_the_ID();
+$heroImage = get_field('hero_image');
+$technicalDescription = get_field('technical_description');
+$website = get_field('website');
+$logo = get_field('logo');
+$students = get_field('students', $project_id);
+$images = get_field('images', $project_id);
 ?>
 
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</head>
+
 <div id="primary" <?php astra_primary_class(); ?>>
-    <div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?php echo esc_url($heroImage['url']) ?>);">
+    <div class="hero-image"
+        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?php echo esc_url($heroImage['url']) ?>);">
         <div class="hero-text">
-            <h1 style="color: white;"><?php echo $title ?></h1>
+            <h1 style="color: white;"><?php echo get_field('title') ?></h1>
         </div>
         <div class="arrow-down">
             <!-- TODO arrow down -->
         </div>
     </div>
 
+
     <div style="height: 100vh;"></div>
 
-    <div style="display: flex;">
-        <div style="flex: 2; padding-right: 1rem;">
-            <h2>
-                <?php echo $subtitle ?>
-            </h2>
+    <section id="project-information">
+        <div class="container mt-3 mb-3 mt-lg-5 mb-lg-5">
+            <div class="row gx-5">
+                <div class="col-12 col-md-7">
+                    <h2 class="mb-3">
+                        <?php echo get_field('subtitle') ?>
+                    </h2>
 
-            <br />
-        
+                    <p>
+                        <?php echo get_field('description') ?>
+                    </p>
+
+                    <?php if ($website):
+                        ?>
+                        <a href="<?php echo $website ?>" target="_blank">
+                            <button>
+                                View Website
+                            </button>
+                        </a>
+                    <?php endif; ?>
+                </div>
+
+                <div class="col-12 col-md-5 d-flex align-items-center mt-5 mt-lg-0">
+                    <img src="<?php echo esc_url($logo['url']) ?>"></img>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php if ($images):
+                        ?>
+    <section id="images">
+        <div class="container">
+            <div class="mt-3 mb-3 mt-lg-5 mb-lg-5" style="width: 100%; position: absolute; left: 5%; height: 500px;">
+                <div class="swiper" style="height: 500px; cursor: grab;">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($images as $image_id):
+                            $url = get_permalink($image_id);
+                            ?>
+                            <div class="swiper-slide" style="height: 500px; width: auto;">
+                                <img src="<?php echo $url ?>"></img>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+                <div class="swiper-pagination" style="position: absolute; left: -5%; margin-bottom: -5vh;">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div style="height: 600px;"></div>
+    <?php endif; ?>
+
+    <section id="technical" class="mt-5  mb-5">
+        <div class="container">
+            <h3 class="mb-2">
+                Going in depth
+            </h3>
+
             <p>
-                <?php echo $description ?>
+                <?php echo $technicalDescription ?>
             </p>
-
-            <a href="<?php echo $website ?>" target="_blank">
-                <button>
-                    View Website
-                </button>
-            </a>
         </div>
-        <div style="flex: 1; align-self: center; margin: 0 1rem;">
-            <img src="<?php echo esc_url($logo['url']) ?>"></img>
-        </div>
-    </div>
-    
-    <!-- TODO replace with padding -->
-    <br />
+    </section>
 
-    <div style="display: flex; justify-content: center;">
-        <h2>Meet the team!</h2>
-    </div>
-    <?php
-        if ( $students ) {
-            foreach( $students as $student_id ) {
-                $student = get_post( $student_id ); 
-                $student_name = get_the_title( $student_id );
-                echo '<p>' . $student_name . '</p>'; 
-            }
-        }
-    ?>
+    <section id="team" class="mt-5">
+        <div class="container">
+            <div class="d-flex justify-content-center mt-5">
+                <h3>Meet the team!</h3>
+            </div>
+
+            <?php foreach ($students as $student_id):
+                $student = get_post($student_id);
+                $student_name = get_the_title($student_id);
+                ?>
+                <div>
+                    <?php echo $student_name ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </section>
+
 </div>
+
+<script>
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: "auto",
+        paginationClickable: true,
+        spaceBetween: 20,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+</script>
