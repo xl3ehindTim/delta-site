09442016f58bbdad072aecb6c5d2b42abb72eaf1
Tim <timbrouwers1@gmail.com>
2024-05-08 08:47:26 +0200

    Added about and projects page

---
+++ wp-content/themes/delta-child/page-about.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="primary" <?php astra_primary_class(); ?>>
+        <div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(http://localhost/delta-site/wp-content/uploads/2024/04/BvOF-GLOW2023_A-Futures-of-GLOW-01-2560x1707-1.jpg);">
+            <div class="hero-text">
+                <h2 style="color: white;">About</h2>
+            </div>
+            <div class="arrow-down">
+                <!-- TODO arrow down -->
+            </div>
+        </div>
+
+        <div style="height: 100vh;"></div>
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/delta-child/page-projects.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Projects
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="primary" <?php astra_primary_class(); ?>>
+    <div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(http://localhost/delta-site/wp-content/uploads/2024/05/feel-the-spark-hero.jpg);">
+        <div class="hero-text">
+            <h2 style="color: white;">Projects</h2>
+        </div>
+        <div class="arrow-down">
+            <!-- TODO arrow down -->
+        </div>
+    </div>
+
+    <div style="height: 100vh;"></div>
+
+    <!-- Content here -->
+</div>
+
+<?php get_footer(); ?>
