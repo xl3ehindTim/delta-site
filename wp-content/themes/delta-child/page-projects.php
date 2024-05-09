0b3703521c705210060e5a1b87aa54d9be71f753
ScriptDeBurger <wardlatten@hotmail.nl>
2024-05-09 13:49:28 +0200

    Added project cards

--- wp-content/themes/delta-child/page-projects.php
+++ wp-content/themes/delta-child/page-projects.php
@@ -7,7 +7,11 @@ Template Name: Projects
 <?php get_header(); ?>
 
 <div id="primary" <?php astra_primary_class(); ?>>
+<<<<<<< Updated upstream
     <div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(http://localhost/delta-site/wp-content/uploads/2024/05/feel-the-spark-hero.jpg);">
+=======
+    <div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(http://localhost/delta-site/wp-content/uploads/2024/03/VC-Glow-03.jpeg);">
+>>>>>>> Stashed changes
         <div class="hero-text">
             <h2 style="color: white;">Projects</h2>
         </div>
@@ -19,6 +23,37 @@ Template Name: Projects
     <div style="height: 100vh;"></div>
 
     <!-- Content here -->
+<<<<<<< Updated upstream
 </div>
 
 <?php get_footer(); ?>
+=======
+    <!-- <div class="container mt-5">
+        <div class="card">
+            <img src="http://localhost/delta-site/wp-content/uploads/2024/04/BvOF-GLOW2023_A-Futures-of-GLOW-01-2560x1707-1.jpg" alt="Avatar" style="width:100%">
+        <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+        </div> -->
+
+<div class="card">
+    <div class="card-image">
+    <!-- Your image goes here -->
+    </div>
+    <div class="title-container">
+      <h2>GLOW 2023</h2>
+      <h3>Future of GLOW</h3>
+    </div>
+    <div class="card-content">
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius dignissim dui, eu efficitur elit faucibus ac. Sed iaculis quam et elit tristique egestas. Donec et tellus condimentum, blandit risus eget, sollicitudin nulla. Suspendisse potenti. Cras vestibulum risus quis bibendum feugiat. Vivamus sed sem justo. Vestibulum gravida ex non libero posuere sollicitudin. Nullam at iaculis dui. Cras et consequat lectus, tristique posuere leo. Sed sollicitudin, dui a efficitur mattis, justo sapien semper lorem, vitae dapibus mauris turpis in augue. Donec ultrices ex sapien, eu malesuada magna fringilla eget. In accumsan scelerisque lacus, non vestibulum urna consectetur in. </p>
+    </div>
+    <div>
+        <button class="button"><span class="button-text">Read more</span><span class="diagonal-arrow"></span></button>
+    </div>
+</div>
+
+    </div> 
+</div>
+
+<?php get_footer(); ?>
+>>>>>>> Stashed changes
