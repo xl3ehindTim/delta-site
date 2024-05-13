373939738778ce64d89b2c54cfde95dcaf747e7c
Tim <timbrouwers1@gmail.com>
2024-05-13 16:27:33 +0200

    Moved code into seperate functions

--- wp-content/themes/delta-child/single-projects.php
+++ wp-content/themes/delta-child/single-projects.php
@@ -15,13 +15,7 @@ if (!defined('ABSPATH')) {
 get_header(); ?>
 
 <?php
-$project_id = get_the_ID();
-$heroImage = get_field('hero_image');
-$technicalDescription = get_field('technical_description');
-$website = get_field('website');
-$logo = get_field('logo');
-$images = get_field('images', $project_id);
-$projectGroups = get_field('project_groups');
+	$projectGroups = get_field('project_groups');
 ?>
 
 <head>
@@ -32,174 +26,20 @@ $projectGroups = get_field('project_groups');
 </head>
 
 <div id="primary" <?php astra_primary_class(); ?>>
-    <!-- Hero -->
-    <div class="hero-image"
-        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?php echo esc_url($heroImage['url']) ?>);">
-        <div class="hero-text">
-            <h1 style="color: white;"><?php echo get_field('title') ?></h1>
-        </div>
-        <!-- <div class="arrow-down">
-            <p style="margin-bottom: -5px; font-weight: 600;">Explore project</p>
-            <i class="arrow down"></i>
-        </div> -->
-    </div>
+    <?php include_hero_section(); ?>
 
+		<?php include_project_information(); ?>
 
-    <div style="height: 100vh;"></div>
+		<?php include_image_slider(); ?>
 
-    <!-- Project info -->
-    <section id="project-information">
-        <div class="container mt-3 mb-3 mt-lg-5 mb-lg-5">
-            <div class="row gx-5">
-                <div class="col-12 col-md-7">
-                    <h2 class="mb-3">
-                        <?php echo get_field('subtitle') ?>
-                    </h2>
+		<?php include_technical_information(); ?>
 
-                    <?php echo get_field('description') ?>
+		<?php include_team_section(); ?>
 
-                    <?php if ($website):
-                        ?>
-                        <a href="<?php echo $website ?>" target="_blank" style="text-decoration: none;">
-                            <button>
-                                View Website
-                                <i class="arrow right"></i>
-                            </button>
-                        </a>
-                    <?php endif; ?>
-                </div>
-
-                <?php if ($logo): ?>
-                    <div
-                        class="col-12 col-md-5 d-flex align-items-center mt-5 mt-lg-0  justify-content-center justify-content-md-end">
-                        <img style="max-width: 300px;" src="<?php echo esc_url($logo['url']) ?>"></img>
-                    </div>
-                <?php endif; ?>
-            </div>
-        </div>
-    </section>
-
-    <!-- Images -->
-    <?php if ($images):
-        ?>
-        <section id="images">
-            <div class="container">
-                <div class="mt-3 mb-3 mt-lg-5 mb-lg-5" style="width: 100%; position: absolute; left: 0%; height: 500px;">
-                    <div class="swiper-images" style="height: 500px;">
-                        <div class="swiper-wrapper">
-                            <?php foreach ($images as $image_id):
-                                $url = get_permalink($image_id);
-                                ?>
-                                <div class="swiper-slide" style="height: 500px; width: auto;">
-                                    <img src="<?php echo $url ?>"></img>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                    <div class="swiper-pagination swiper-images-pagination"
-                        style="position: absolute; margin-bottom: -5vh;">
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <div style="height: 600px;"></div>
-    <?php endif; ?>
-
-    <!-- Technical description -->
-    <?php if ($technicalDescription): ?>
-        <section id="technical" class="mt-3 mb-3 mt-lg-5 mb-lg-5">
-            <div class="container">
-                <h3 class="mb-2">
-                    Going in depth
-                </h3>
-
-                <?php echo $technicalDescription ?>
-            </div>
-        </section>
-    <?php endif; ?>
-
-    <!-- Team -->
-    <?php if ($projectGroups): ?>
-        <section id="team">
-            <div class="container">
-                <div class="d-flex justify-content-center mt-5">
-                    <h3>Meet the team!</h3>
-                </div>
-
-                <?php if (count($projectGroups) > 1): ?>
-                    <div class="d-flex flex-row justify-content-center">
-                        <?php foreach ($projectGroups as $projectGroup):
-                            $projectGroupId = $projectGroup->ID;
-                            $publishedAt = new DateTime(get_post_field('post_date', $projectGroupId));
-                            $publishedYear = $publishedAt->format('Y');
-                            $publishedMonth = $publishedAt->format('m');
-
-                            // Determine semester based on published month
-                            $semester = ($publishedMonth <= 6) ? 'Spring' : 'Fall';
-                            ?>
-
-                            <div class="p-2 bd-highlight term-selection" style="cursor: pointer;" data-id="<?php echo $projectGroupId ?>">
-                                <?php echo $semester . ' ' . $publishedYear ?>
-                            </div>
-
-                        <?php endforeach ?>
-                    </div>
-                <?php endif; ?>
-
-                <div class="mt-3 mb-3 mt-lg-5 mb-lg-5" style="width: 100%; position: absolute; left: 0%; height: 225px;">
-                    <div class="swiper-container" style="height: 225px;">
-                        <div class="swiper-wrapper">
-                            <!-- Dynamic Content -->
-                        </div>
-                    </div>
-                    <div class="swiper-pagination swiper-container-pagination"
-                        style="position: absolute; margin-bottom: -5vh;">
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <div style="height: 300px;"></div>
-    <?php endif; ?>
-
-    <!-- Previous and next -->
-    <div class="d-flex justify-content-between mt-5">
-        <div>
-            <?php echo next_post_link('%link', '<button><i class="arrow left"></i>' . get_the_title(get_next_post()) . '</button>'); ?>
-        </div>
-        <div>
-            <?php echo previous_post_link('%link', '<button>' . get_the_title(get_previous_post()) . '<i class="arrow right"></i></button>'); ?>
-        </div>
-    </div>
+		<?php include_page_navigation(); ?>
 </div>
 
 <?php get_footer() ?>
-
-<script>
-    var imageSwiper = new Swiper(".swiper-images", {
-        slidesPerView: 2,
-        spaceBetween: 20,
-        pagination: {
-            el: ".swiper-images-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            // width => 320px 
-            320: {
-                slidesPerView: 'auto',
-                slidesOffsetBefore: 20,
-                slidesOffsetAfter: 20,
-            },
-            // width => 999px 
-            999: {
-                spaceBetweenSlides: 50,
-                slidesOffsetBefore: 100,
-                slidesOffsetAfter: 100,
-            }
-        }
-    });
-</script>
 
 <script>
     /**
@@ -289,3 +129,196 @@ $projectGroups = get_field('project_groups');
         });
     });
 </script>
+
+<?php
+/**
+ * Section functions
+ */
+
+function include_hero_section() {
+		$hero_image = get_field('hero_image');
+    ?>
+    <div class="hero-image"
+      style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?php echo esc_url($hero_image['url']); ?>);">
+      <div class="hero-text">
+        <h1 style="color: white;"><?php echo get_field('title'); ?></h1>
+      </div>
+      </div>
+  
+    <div style="height: 100vh;"></div>
+    <?php
+  }
+
+	function include_project_information() {
+		$subtitle = get_field('subtitle');
+    $description = get_field('description');
+    $website = get_field('website');
+		$logo = get_field('logo');
+		?>
+			<div class="project-info container mt-3 mb-3 mt-lg-5 mb-lg-5">
+				<div class="row gx-5">
+					<div class="col-12 col-md-7">
+						<h2 class="mb-3">
+							<?php echo $subtitle; ?>
+						</h2>
+						<div>
+							<?php echo $description; ?>
+						</div>
+  
+						<?php if ($website): ?>
+							<a href="<?php echo $website; ?>" target="_blank" style="text-decoration: none;">
+								<button>
+									View Website
+									<i class="arrow right"></i>
+								</button>
+							</a>
+						<?php endif; ?>
+					</div>
+		
+						<?php if ($logo): ?>
+							<div class="col-12 col-md-5 d-flex align-items-center mt-5 mt-lg-0 justify-content-center justify-content-md-end">
+							<img style="max-width: 300px;" src="<?php echo esc_url($logo['url']) ?>" alt="Project Logo" />
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		<?php
+	}
+
+	function include_image_slider() {
+		$images = get_field('images');
+		if (!$images) {
+			return; 
+		}
+		?>
+			<section id="images">
+            <div class="container">
+                <div class="mt-3 mb-3 mt-lg-5 mb-lg-5" style="width: 100%; position: absolute; left: 0%; height: 500px;">
+                    <div class="swiper-images" style="height: 500px;">
+                        <div class="swiper-wrapper">
+                            <?php foreach ($images as $image_id):
+                                $url = get_permalink($image_id);
+                                ?>
+                                <div class="swiper-slide" style="height: 500px; width: auto;">
+                                    <img src="<?php echo $url ?>"></img>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination swiper-images-pagination"
+                        style="position: absolute; margin-bottom: -5vh;">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+				<div style="height: 600px;"></div>
+
+				<script>
+    /**
+     * Image swiper
+     */
+    var imageSwiper = new Swiper(".swiper-images", {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        pagination: {
+            el: ".swiper-images-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            // width => 320px 
+            320: {
+                slidesPerView: 'auto',
+                slidesOffsetBefore: 20,
+                slidesOffsetAfter: 20,
+            },
+            // width => 999px 
+            999: {
+                spaceBetweenSlides: 50,
+                slidesOffsetBefore: 100,
+                slidesOffsetAfter: 100,
+            }
+        }
+    });
+</script>
+		<?php
+	}
+
+function include_technical_information() {
+	$technical_description = get_field('technical_description');
+	if (!$technical_description) {
+		return;
+	}
+
+	?>
+		<section id="technical" class="mt-3 mb-3 mt-lg-5 mb-lg-5">
+			<div class="container">
+				<h3 class="mb-2">
+					Going in depth
+				</h3>
+
+				<?php echo $technical_description ?>
+			</div>
+		</section>
+	<?php
+}
+
+function include_page_navigation() {
+	?>
+    <div class="d-flex justify-content-between mt-5">
+        <div>
+            <?php echo next_post_link('%link', '<button><i class="arrow left"></i>' . get_the_title(get_next_post()) . '</button>'); ?>
+        </div>
+        <div>
+            <?php echo previous_post_link('%link', '<button>' . get_the_title(get_previous_post()) . '<i class="arrow right"></i></button>'); ?>
+        </div>
+    </div>
+	<?php
+}
+function include_team_section() {
+	$project_groups = get_field('project_groups');
+	if (!$project_groups) return;
+
+	?>
+		<section id="team">
+			<div class="container">
+				<div class="d-flex justify-content-center mt-5">
+						<h3>Meet the team!</h3>
+				</div>
+
+				<!-- Term selection if > 1 groups -->
+				<?php if (count($project_groups) > 1): ?>
+					<div class="d-flex flex-row justify-content-center">
+						<?php foreach ($project_groups as $projectGroup):
+							$projectGroupId = $projectGroup->ID;
+							$publishedAt = new DateTime(get_post_field('post_date', $projectGroupId));
+							$publishedYear = $publishedAt->format('Y');
+							$publishedMonth = $publishedAt->format('m');
+
+							// Determine semester based on published month
+							$semester = ($publishedMonth <= 6) ? 'Spring' : 'Fall';
+						?>
+							<div class="p-2 bd-highlight term-selection" style="cursor: pointer;" data-id="<?php echo $projectGroupId ?>">
+								<?php echo $semester . ' ' . $publishedYear ?>
+							</div>
+						<?php endforeach ?>
+					</div>
+				<?php endif; ?>
+
+				<div class="mt-3 mb-3 mt-lg-5 mb-lg-5" style="width: 100%; position: absolute; left: 0%; height: 225px;">
+					<div class="swiper-container" style="height: 225px;">
+						<div class="swiper-wrapper">
+							<!-- Dynamic Content -->
+						</div>
+					</div>
+					<div class="swiper-pagination swiper-container-pagination"
+						style="position: absolute; margin-bottom: -5vh;">
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div style="height: 300px;"></div>
+	<?php
+}
+?>
