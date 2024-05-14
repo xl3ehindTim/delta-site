3dc5e8fc77d63d867609223025c3607091bd99dd
Tim <timbrouwers1@gmail.com>
2024-05-14 10:45:52 +0200

    Load bootstrap and swiperjs in functions.php

--- wp-content/themes/delta-child/functions.php
+++ wp-content/themes/delta-child/functions.php
@@ -20,6 +20,13 @@ function child_enqueue_styles() {
 
 	wp_enqueue_style( 'delta-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_DELTA_VERSION, 'all' );
 
+	// Bootstrap
+	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', array(), '5.2.3' );
+
+	// Swiperjs
+	wp_enqueue_style( 'swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11' ); 
+	wp_enqueue_script( 'swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11' ); 
+
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
