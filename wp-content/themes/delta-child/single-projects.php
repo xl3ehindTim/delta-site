4f233725fc0045b133278c58d8bda98d35d6910e
Tim <timbrouwers1@gmail.com>
2024-05-24 14:26:57 +0200

    Removed muted on video

--- wp-content/themes/delta-child/single-projects.php
+++ wp-content/themes/delta-child/single-projects.php
@@ -208,7 +208,7 @@ function include_hero_section() {
 
                                     if (wp_attachment_is( 'video', $media_id)) {
                                         ?>
-                                            <video style="height: 500px; width: auto;" controls muted src="<?php echo $url ?>"></video>
+                                            <video style="height: 500px; width: auto;" controls src="<?php echo $url ?>"></video>
                                         <?php
                                     }
                                 ?>
