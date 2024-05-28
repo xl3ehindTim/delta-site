ac3f472456de0595e6e0c1d029ccb6f098c3615e
Tim <timbrouwers1@gmail.com>
2024-05-28 09:20:48 +0200

    Fix loop empty div

--- wp-content/themes/delta-child/single-student.php
+++ wp-content/themes/delta-child/single-student.php
@@ -2,6 +2,7 @@
 $args = array(
     'post_type' => 'project-group',
     'posts_per_page' => 100,
+    'post_status' => 'publish',
     'meta_query' => array(
         array(
             'key' => 'students',
@@ -23,7 +24,8 @@ $project_groups = new WP_Query($args);
 
 
 <div id="primary" <?php astra_primary_class(); ?>>
-<div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../../wp-content/uploads/static/home-team.jpg);">
+    <div class="hero-image"
+        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../../wp-content/uploads/static/home-team.jpg);">
         <div class="hero-text">
             <h1 style="color: white;">
                 <?php echo get_the_title(); ?>
@@ -42,78 +44,67 @@ $project_groups = new WP_Query($args);
             <h2>
                 Participated projects
             </h2>
+
             <div class="row projects-container mt-5">
-
-                <?php if ($project_groups->have_posts()): ?>
-                    <?php while ($project_groups->have_posts()):
-                        $project_groups->the_post();
-
-                        // Get projects linked to project group
-                        $projects = new WP_Query(
-                            array(
-                                'post_type' => 'projects',
-                                'posts_per_page' => 100,
-                                'meta_query' => array(
-                                    array(
-                                        'key' => 'project_groups',
-                                        'value' => get_the_ID(),
-                                        'compare' => 'LIKE',
-                                    ),
+                <?php foreach ($project_groups->posts as $post):
+                    // Get projects linked to project group
+                    $projects = new WP_Query(
+                        array(
+                            'post_type' => 'projects',
+                            'posts_per_page' => 100,
+                            'post_status' => 'publish',
+                            'meta_query' => array(
+                                array(
+                                    'key' => 'project_groups',
+                                    'value' => get_the_ID(),
+                                    'compare' => 'LIKE',
                                 ),
-                            )
-                        );
+                            ),
+                        )
+                    );
+                    ?>
+                    <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+                        <?php foreach ($projects->posts as $post):
 
-                        while ($projects->have_posts()) {
-                            $projects->the_post();
                             ?>
-                            <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="card text-dark card-has-bg click-col">
-                                        <img style="height: 550px; object-fit: cover;" class="card-img"
-                                            src="<?php echo esc_url(get_field('hero_image')['url']) ?>">
-                                        <div class="card-img-overlay d-flex flex-column">
-                                            <div id="card-footer" class="justify-content-between" style="display: flex;">
-                                                <div class="card-footer-text">
-                                                    <h6 class="my-0 text-white d-block" style="font-size:22px;">
-                                                        <?php echo get_the_title(); ?>
-                                                    </h6>
-                                                    <p class="text-white" style="font-size:16px;">
-                                                        <?php echo get_field('subtitle') ?>
-                                                    </p>
-                                                </div>
-                                                <div class="card-footer-placeholder"></div>
-                                                <div class="card-button">
-                                                    <span class="card-button-text">
-                                                        <h6>Read more</h6>
-                                                    </span>
-                                                    <span class="diagonal-arrow"></span>
-                                                </div>
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="card text-dark card-has-bg click-col">
+                                    <img style="height: 550px; object-fit: cover;" class="card-img"
+                                        src="<?php echo esc_url(get_field('hero_image')['url']) ?>">
+                                    <div class="card-img-overlay d-flex flex-column">
+                                        <div id="card-footer" class="justify-content-between" style="display: flex;">
+                                            <div class="card-footer-text">
+                                                <h6 class="my-0 text-white d-block" style="font-size:22px;">
+                                                    <?php echo get_the_title(); ?>
+                                                </h6>
+                                                <p class="text-white" style="font-size:16px;">
+                                                    <?php echo get_field('subtitle') ?>
+                                                </p>
                                             </div>
-                                            <div class="card-description">
-                                                <?php
-                                                // Shorten description to 700 characters and add ellipsis
-                                                $shortDescription = substr(get_field('description'), 0, 700);
-                                                if (strlen(get_field('description')) > 700) {
-                                                    $shortDescription .= "...";
-                                                }
-                                                echo $shortDescription;
-                                                ?>
+                                            <div class="card-footer-placeholder"></div>
+                                            <div class="card-button">
+                                                <span class="card-button-text">
+                                                    <h6>Read more</h6>
+                                                </span>
+                                                <span class="diagonal-arrow"></span>
                                             </div>
                                         </div>
+                                        <div class="card-description">
+                                            <?php
+                                            // Shorten description to 700 characters and add ellipsis
+                                            $shortDescription = substr(get_field('description'), 0, 700);
+                                            if (strlen(get_field('description')) > 700) {
+                                                $shortDescription .= "...";
+                                            }
+                                            echo $shortDescription;
+                                            ?>
+                                        </div>
                                     </div>
-                                </a>
-                            </div>
-                            <?php
-                            wp_reset_postdata();
-                        }
-                        ?>
-
-                        <div>
-                        </div>
-                    <?php endwhile ?>
-                <?php else: ?>
-                <?php endif ?>
-
+                                </div>
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
