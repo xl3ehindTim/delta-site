47ed1e65ef03e7bce71e0e6d180198caf8fe2585
Tim <timbrouwers1@gmail.com>
2024-05-31 11:40:31 +0200

    Added SEO plugin
    Co-Authored-By: Bas Bakens <76151181+Bash-04@users.noreply.github.com>

---
+++ wp-content/plugins/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '94db08a5e45b0c74a17c00051dfb3423535615b7',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'yoast/whip' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yoast/whip',
+            'aliases' => array(),
+            'reference' => '5cfd9c3b433774548ec231fe896d5e85d17ed0d1',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '94db08a5e45b0c74a17c00051dfb3423535615b7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
